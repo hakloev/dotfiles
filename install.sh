e6d95d050dffed2462115f81694f489863dee6ba
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-01-21 18:08:00 +0100

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Add -x after first line for debug mode
 
-for file in bash_profile gitconfig gitexcludes screenrc vimrc zshrc; do
+for file in bash_profile gitconfig gitexcludes screenrc vimrc zshrc vim; do
     rm -f ~/.$file
     ln -s $PWD/$file ~/.$file;
 done
@@ -10,13 +10,8 @@ done
 touch ~/.zsh_history
 touch ~/.bash_history
 
-echo "Installed all dot-files, but did not move .git and .vim"
 echo "Will now generate ssh-public key"
 
 ssh-keygen
-
-echo "Installing Homebrew"
-
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 echo "Remember the sudo-command with htop"
