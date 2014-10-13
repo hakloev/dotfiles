8ab74f11ca97098d26403f8ce01a141697fc125a
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-10-13 14:06:34 +0200

    osx stuff

--- deployAll.sh
+++ deployAll.sh
@@ -18,22 +18,7 @@ function install_brew() {
 
 function config_osx() {
     echo "Setting OS X spesific settings"
-    # Always open everything in Finder's list view. This is important.
-    defaults write com.apple.Finder FXPreferredViewStyle Nlsv
-    # Show the ~/Library folder.
-    chflags nohidden ~/Library
-    # Set a really fast key repeat.
-    defaults write NSGlobalDomain KeyRepeat -int 0
-    # Set the Finder prefs for showing a few different volumes on the Desktop.
-    defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-    defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-    # Set up Safari for development.
-    defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-    defaults write com.apple.Safari IncludeDevelopMenu -bool true
-    defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-    defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-    defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-    killall Finder
+    source ~/git/unix-dot/osx_config
 }
 
 function set_motd() {
