8921dfb7a1e6775761cef7bb7fe2d9f26c8d7869
Håkon Ødegård Løvdal <post@hakloev.no>
2017-11-02 20:58:19 +0100

    Update vscode dep

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 extensions=(
-  "EditorConfig.EditorConfig"
-  "dbaeumer.vscode-eslint"
-  "sbrink.elm"
+    "EditorConfig.EditorConfig"
+    "christian-kohler.path-intellisense"
+    "dbaeumer.vscode-eslint"
+    "dustinsanders.an-old-hope-theme-vscode"
+    "iGN97.fuse-vscode"
+    "jakob101.RelativePath"
+    "ms-vscode.node-debug2"
+    "naumovs.color-highlight"
+    "robertohuertasm.vscode-icons"
+    "sbrink.elm"
+    "vsmobile.vscode-react-native"
+
 )
 
 for extension in ${extensions[@]}
