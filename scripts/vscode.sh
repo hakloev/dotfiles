5aecdb4aa78278f32c7858edad2d225400879329
Håkon Ødegård Løvdal <post@hakloev.no>
2017-11-11 12:24:22 +0100

    Update vscode ext

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 extensions=(
+    "DotJoshJohnson.xml"
     "EditorConfig.EditorConfig"
+    "aaronthomas.vscode-snazzy-operator"
+    "christian-kohler.npm-intellisense"
     "christian-kohler.path-intellisense"
     "dbaeumer.vscode-eslint"
     "dustinsanders.an-old-hope-theme-vscode"
@@ -9,10 +12,12 @@ extensions=(
     "jakob101.RelativePath"
     "ms-vscode.node-debug2"
     "naumovs.color-highlight"
+    "redhat.java"
     "robertohuertasm.vscode-icons"
     "sbrink.elm"
+    "vscjava.vscode-java-debug"
+    "vscjava.vscode-java-pack"
     "vsmobile.vscode-react-native"
-
 )
 
 for extension in ${extensions[@]}
