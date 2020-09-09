9df3c8e007a027b2fe938fa2e42ce583ed893ae3
Håkon Ødegård Løvdal <hakon@loevdal.no>
2020-09-09 09:03:13 +0200

    Create screenshot dirs

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -13,5 +13,12 @@ fi
 
 sudo -v
 
+screenshot_dir=$HOME/Desktop/Screenshots
+
+mkdir -p $screenshot_dir/{iOS,macOS}
+
+defaults write com.apple.iphonesimulator ScreenShotSaveLocation "$screenshot_dir/iOS"
+defaults write com.apple.screencapture location "$screenshot_dir/macOS"
+
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
