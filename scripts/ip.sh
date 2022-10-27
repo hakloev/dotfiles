6da9f4def59205eea1a18e6d16d4d5e8137aaef0
Håkon Ødegård Løvdal <hakon@loevdal.no>
2022-10-27 14:53:19 +0200

    Minimize output

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 ACTIVE_INTERFACE=$(route get 0.0.0.0 2>/dev/null | awk '/interface: /{print $2}')
 LOCAL_IP=$(ifconfig -r $ACTIVE_INTERFACE | awk '/inet /{print $2}')
@@ -6,7 +6,7 @@ LOCAL_IP=$(ifconfig -r $ACTIVE_INTERFACE | awk '/inet /{print $2}')
 PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 if [ -z "${PUBLIC_IP}" ] || [ "${LOCAL_IP}" == "${PUBLIC_IP}" ]; then
-  echo "IP: ${LOCAL_IP}"
+  echo "${LOCAL_IP}"
 else
-  echo "IP: ${LOCAL_IP} / ${PUBLIC_IP}"
+  echo "${LOCAL_IP}/${PUBLIC_IP}"
 fi
