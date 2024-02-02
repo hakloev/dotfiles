8d0b7fcfe4bf45fb3a166e16a5544b0e1319092a
Håkon Ødegård Løvdal <hakon@loevdal.no>
2024-02-02 19:49:01 +0100

    refactor: use env

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 ACTIVE_INTERFACE=$(route get 0.0.0.0 2>/dev/null | awk '/interface: /{print $2}')
 LOCAL_IP=$(ifconfig -r $ACTIVE_INTERFACE | awk '/inet /{print $2}')
@@ -8,5 +8,5 @@ PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 if [ -z "${PUBLIC_IP}" ] || [ "${LOCAL_IP}" == "${PUBLIC_IP}" ]; then
   echo "${LOCAL_IP}"
 else
-  echo "${LOCAL_IP}/${PUBLIC_IP}"
+  echo "${LOCAL_IP} / ${PUBLIC_IP}"
 fi
