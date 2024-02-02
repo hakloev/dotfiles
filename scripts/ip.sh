defa0b0195fe5c33842baf8156cd728e5f2ceff2
Håkon Ødegård Løvdal <hakon@loevdal.no>
2024-02-02 19:58:34 +0100

    feat: show local ip in tmux

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+kind=${1:-internal}
+
 ACTIVE_INTERFACE=$(route get 0.0.0.0 2>/dev/null | awk '/interface: /{print $2}')
 LOCAL_IP=$(ifconfig -r $ACTIVE_INTERFACE | awk '/inet /{print $2}')
 
-PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-if [ -z "${PUBLIC_IP}" ] || [ "${LOCAL_IP}" == "${PUBLIC_IP}" ]; then
-  echo "${LOCAL_IP}"
-else
-  echo "${LOCAL_IP} / ${PUBLIC_IP}"
-fi
+case $kind in
+  internal)
+    echo $LOCAL_IP
+  ;;
+  external)
+    PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+    echo $PUBLIC_IP
+  ;;
+esac
